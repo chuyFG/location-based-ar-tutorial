7d43b092c1b3e8acb7515b6a44268651776dd8cd
Jesus Flores Guerra <chuyfloresguerra@gmail.com>
2021-04-30 08:37:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 39.575947,
+                lng: -104.865736,
             },
         },
     ];
